2ec7ef21c70fbca96091782d9976c1da6e055c0b
xyysjd <91957166+xyysjd@users.noreply.github.com>
2023-02-08 17:45:18 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -17,13 +17,19 @@ fi
 if [ "$OS" == "ubuntu" ]; then
     # Ubuntu或Debian
     sudo apt-get update
-    sudo apt-get install -y docker.io
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
     sudo systemctl start docker
     sudo systemctl enable docker
 elif [ "$OS" == "centos" ]; then
     # CentOS
     sudo yum update -y
-    sudo yum install -y docker
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce docker-ce-cli containerd.io
     sudo systemctl start docker
     sudo systemctl enable docker
 fi
