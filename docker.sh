d439c6575c4c26406dcc8c5c158cf5ec2c5ea603
xyysjd <91957166+xyysjd@users.noreply.github.com>
2023-02-08 17:52:15 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -13,32 +13,51 @@ else
     exit 1
 fi
 
-# 安装Docker的步骤
-if [ "$OS" == "ubuntu" ]; then
-    # Ubuntu或Debian
-    sudo apt-get update
-    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
-elif [ "$OS" == "centos" ]; then
-    # CentOS
-    sudo yum update -y
-    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    sudo yum install -y docker-ce docker-ce-cli containerd.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
-fi
+# 安装或卸载的选项
+OPTION=$1
 
-# 检查Docker是否安装成功
-if [ "$(docker version 2>&1 >/dev/null)" ]; then
-    echo "Docker installed successfully!"
-    echo "Docker version: $(docker version --format '{{.Server.Version}}')"
+# 安装Docker的步骤
+if [ "$OPTION" == "install" ]; then
+    if [ "$OS" == "ubuntu" ]; then
+        # Ubuntu或Debian
+        sudo apt-get update
+        sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+        sudo systemctl start docker
+        sudo systemctl enable docker
+    elif [ "$OS" == "centos" ]; then
+        # CentOS
+        sudo yum update -y
+        sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+        sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        sudo yum install -y docker-ce docker-ce-cli containerd.io
+        sudo systemctl start docker
+        sudo systemctl enable docker
+    fi
+    # 检查Docker是否安装成功
+    if [ "$(docker version 2>&1 >/dev/null)" ]; then
+        echo "Docker installed successfully!"
+        echo "Docker version: $(docker version --format '{{.Server.Version}}')"
+    else
+        echo "Error: Failed to install Docker."
+        exit 1
+    fi
+elif [ "$OPTION" == "uninstall" ]; then
+    if [ "$OS" == "ubuntu" ]; then
+        # Ubuntu或Debian
+        sudo systemctl stop docker
+        sudo apt-get remove -y docker-ce docker-ce-cli containerd.io
+        sudo apt-get autoremove -y
+    elif [ "$OS" == "centos" ]; then
+        # CentOS
+        sudo systemctl stop docker
+        sudo yum remove -y docker-ce docker-ce-cli containerd.io
+    fi
+    echo "Docker uninstalled successfully!"
 else
-    echo "Error: Failed to install Docker."
+    echo "Error: Invalid option. Usage: $0 [install|uninstall]"
     exit 1
 fi
