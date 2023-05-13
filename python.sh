d9896e4c723c0904efc3f42dbaeb08cb7abd1b98
xyysjd <91957166+xyysjd@users.noreply.github.com>
2023-05-13 13:58:37 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -5,13 +5,13 @@ sudo apt-get update
 sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
 
 # 下载Python源码包
-wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
+wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 
 # 解压缩源码包
-tar xvf Python-3.11.3.tgz
+tar xvf Python-3.10.0.tgz
 
 # 进入解压目录
-cd Python-3.11.3
+cd Python-3.10.0
 
 # 编译并安装Python
 ./configure --enable-optimizations
@@ -19,7 +19,7 @@ make -j $(nproc)
 sudo make install
 
 # 更新符号链接
-sudo ln -sf /usr/local/bin/python3.11 /usr/bin/python3
+sudo ln -sf /usr/local/bin/python3.10 /usr/bin/python3
 
 # 验证Python版本
 python3 --version
